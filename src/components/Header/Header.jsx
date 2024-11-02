b3d82d45b9bf56cfef4b7e2932180b40532c8db5
tawfiq <tawfiqealahi99@gmail.com>
2024-11-03 04:41:43 +0600

    tailwind setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import  './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+             <h1 className="text-3xl color-blue font-semibold underline">
+      Hello world!
+    </h1>h
+        </div>
+    );
+};
+
+export default Header;
